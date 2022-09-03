f0ad538597a0f95b46b1294c94cb68e4c30221ec
Pooja123jha <112758251+Pooja123jha@users.noreply.github.com>
2022-09-03 08:34:53 -0400

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Cls
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a,b,c;
+System.out.println("enter two values");
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+System.out.println(c);
+}
